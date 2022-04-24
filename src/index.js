0eee23314e39140e20d1bc59a67ca2ed361d5b95
Kanoj Vora <kkvsith@gmail.com>
2022-04-24 21:13:10 +0100

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import moment from "moment"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Tweet({tweet}){
+    return (
+        <div className="tweet">
+            <Avatar hash={tweet.gravatar}/>
+            <div className="content">
+                <Author author={tweet.author}/><Time time={tweet.timestamp}/>
+                <Message text={tweet.message}/>
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton count={tweet.retweets}/>
+                    <LikeButton count={tweet.likes}/>
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Avatar({hash}){
+    const url = `https://www.gravatar.com/avatar/${hash}`
+    return (
+        <img 
+            src={url}
+            className="avatar"
+            alt="avatar"
+        />
+    )
+}
+
+function Message({text}){
+    return (
+        <div className="message">
+            {text}
+        </div>
+    )
+}
+
+function Author({author}){
+    const {name, handle} = author;
+    return (
+        <span className="author">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+        </span>
+    )
+}
+
+const Time = ({time}) => {
+    const timeString = moment(time).fromNow()
+    return (
+        <span className="time">{timeString}</span>
+    )
+}
+
+const ReplyButton = ({count}) => (
+    <span>
+        <i className="fa fa-reply reply-button"/>
+        {getRetweetCount(count)}
+    </span>
+)
+
+const RetweetButton = () => (
+    <i className="fa fa-retweet retweet-button"/>
+)
+
+const LikeButton = ({count}) => (
+    <span>
+        <i className="fa fa-heart like-button"/>
+        {count > 0 &&
+            <span className="like-count">
+                {count}
+            </span>
+        }
+    </span>
+)
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button"/>
+)
+
+const testTweet = {
+    message: 'This is a tweet',
+    gravatar: 'xyz',
+    author: {
+        handle: 'tweeter',
+        name: 'Elon Musk'
+    },
+    likes: 2,
+    retweets: 0,
+    timestamp: '2022-07-10 21:24:30'
+}
+
+function getRetweetCount(count){
+    if(count > 0) {
+        <span className="retweet-count">
+            {count}
+        </span>
+    } else {
+        return null
+    }
+}
+
+ReactDOM.render(
+    <Tweet tweet={testTweet} />,
+    document.querySelector('#root')
+)
